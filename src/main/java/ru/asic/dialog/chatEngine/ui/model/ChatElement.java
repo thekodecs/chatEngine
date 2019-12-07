bfad9c9d295a7d51488e730944de6b3ff5ac3597
Kirill_Al <kirillamsv@gmail.com>
2019-12-07 21:34:46 +0300

    enum deleted

--- src/main/java/ru/asic/dialog/chatEngine/ui/model/ChatElement.java
+++ src/main/java/ru/asic/dialog/chatEngine/ui/model/ChatElement.java
@@ -1,20 +1,20 @@
 package ru.asic.dialog.chatEngine.ui.model;
 
 public class ChatElement {
-    private TypeOfElement typeOfElement;
+    private String type;
     private String id;
     private String text;
     private String[] callers;
 
-    public ChatElement(TypeOfElement typeOfElement, String id, String text, String[] callers) {
-        this.typeOfElement = typeOfElement;
+    public ChatElement(String type, String id, String text, String[] callers) {
+        this.type = type;
         this.id = id;
         this.text = text;
         this.callers = callers;
     }
 
-    public TypeOfElement getTypeOfElement() {
-        return typeOfElement;
+    public String getType() {
+        return type;
     }
 
     public String getId() {
